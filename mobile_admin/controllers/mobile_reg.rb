cbf471e3dba0de825ca06aaa73fdfd95ed68ba6d
simon <xsqfeather@163.com>
2014-11-17 18:23:51 +0800

    备份回家2014/11/17

--- mobile_admin/controllers/mobile_reg.rb
+++ mobile_admin/controllers/mobile_reg.rb
@@ -1,8 +1,8 @@
 Fancyshpv2::MobileAdmin.controllers :mobile_reg do
    get :index do
-     render('sign_up', :layout => 'mobile_admin') 
+     render('sign_up') 
     end
    post  :create  do
-     
+      render('agency_intro', :layout => 'mobile_admin')
    end
 end
