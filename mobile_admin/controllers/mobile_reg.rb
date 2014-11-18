e4f155059f9a174f91bed580e252762162dab68d
simon <xsqfeather@163.com>
2014-11-18 11:07:16 +0800

    routie test

--- mobile_admin/controllers/mobile_reg.rb
+++ mobile_admin/controllers/mobile_reg.rb
@@ -6,16 +6,24 @@ Fancyshpv2::MobileAdmin.controllers :mobile_reg do
      @account = Account.new(params[:account])
     if @account.save
     	if params[:role] .to_s== 'agency'
-    	    render('agency_intro', :layout => 'mobile_admin')
+    	    redirect_to(url(:mobile_reg, :agency_intro ))
     	elsif params[:role].to_s == 'provider'
-    	    render('approve_intro', :layout => 'mobile_admin')
+    	    redirect_to(url(:mobile_reg, :agency_intro ))
     	else
     	     flash.now[:error] = '提交信息有误(没有role)'
-                    redirect_to(url(:mobile_reg, :index))
+                    redirect_to(url(:mobile_reg, :iagency_intro ))
     	end
-    else
-      flash.now[:error] = '提交信息有误'
-      redirect_to(url(:mobile_reg, :index))
+     else
+       flash.now[:error] = '提交信息有误'
+       redirect_to(url(:mobile_reg, :agency_intro ))
+    end
    end
- end
+
+   get :agency_intro do
+      render('agency_intro', :layout => 'mobile_admin')
+   end
+
+   get :approve_intro do
+       render('approve_intro', :layout => 'mobile_admin')
+   end
 end
