0ed3b30e6077f04144a4bfc52893ceb022194891
simon <xsqfeather@163.com>
2014-12-01 11:34:51 +0800

    mobile_login_sussess

--- mobile_admin/controllers/mobile_login.rb
+++ mobile_admin/controllers/mobile_login.rb
@@ -3,6 +3,14 @@ Fancyshpv2::MobileAdmin.controllers :mobile_login do
     render('login', :layout => 'mobile_admin') 
   end
   get :get_account do
-  	params[:username].to_json
+    if account = Account.authenticate(params[:email], params[:password])
+     account.to_json
+    elsif Padrino.env == :development && params[:bypass]
+      account = Account.first
+       account.to_json
+    else
+       "密码或用户名错误".to_json
+    end
+
   end
 end
