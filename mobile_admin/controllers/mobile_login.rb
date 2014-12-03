02341b84154220b3dcec5320c341cfc332b4da07
simon <xsqfeather@163.com>
2014-12-03 15:30:40 +0800

    修改:         admin/views/accounts/index.erb 	修改:         mobile_admin/controllers/mobile_login.rb

--- mobile_admin/controllers/mobile_login.rb
+++ mobile_admin/controllers/mobile_login.rb
@@ -2,7 +2,7 @@ Fancyshpv2::MobileAdmin.controllers :mobile_login do
   use Rack::Cors do
   allow do
     # put real origins here
-    origins '*'
+    origins '*', 'null'
     # and configure real resources here
     resource '*', :headers => :any, :methods => [:get, :post, :options]
   end
@@ -69,12 +69,10 @@ end
     @account =Account.where(:_id => params[:userid]).first;
     if @account
          @credit_info = CreditInfo.new(:name => params[:p_principal], :email => params[:p_email], :card_id => params[:p_iden], :province_id => params[:p_province], :city_id => params[:p_city], :area_id => params[:p_area])
-     if @credit_info.save
-         @account.updateAttribute(:credit_info_id =>@credit_info._id)
-        ＠credit_info.to_json
-     else
-         '保存未成功'.to_json
-     end
+         @credit_info.save
+         @account.credit_info_id=@credit_info._id
+         @account.save
+         @credit_info.to_json
     else
       1.to_json
     end
