c109af08a1bdab8ec9631721baa0192d0e2cecf8
simon <xsqfeather@163.com>
2014-12-03 14:38:43 +0800

    ddd

--- mobile_admin/controllers/mobile_login.rb
+++ mobile_admin/controllers/mobile_login.rb
@@ -68,7 +68,7 @@ end
   post :update_account_url, :csrf_protection => false  do
     @account =Account.where(:_id => params[:userid]).first;
     if @account
-         @credit_info = CreditInfo.new(:name => params[:p_principal], :email => params[:p_email], :card_id => params[:p_iden], :province_id => params[:p_province], :city_id => params[:p_city], :area_id => params[:p_area], :url => params[:url])
+         @credit_info = CreditInfo.new(:name => params[:p_principal], :email => params[:p_email], :card_id => params[:p_iden], :province_id => params[:p_province], :city_id => params[:p_city], :area_id => params[:p_area])
      if @credit_info.save
          @account.updateAttribute(:credit_info_id =>@credit_info._id)
         ＠credit_info.to_json
