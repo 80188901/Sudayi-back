033ba15bf4f7554a40c61db621f06526267bfd26
simon <xsqfeather@163.com>
2014-12-09 11:46:28 +0800

    uploader cover

--- app/controllers/product_uploader.rb
+++ app/controllers/product_uploader.rb
@@ -18,6 +18,17 @@ end
    @image_item.save
    @image_item.to_json
   end
+  
+    post :create_cover,  :csrf_protection => false  do
+   @image_item = ImageItem.new
+   @image_item.product_id = params[:product_id]
+   @image_item.url = params[:cover_url]
+   @image_item.name = '封面'
+   @image_item.is_cover = 1
+   @image_item.save
+   @image_item.to_json
+  end
+
 
     get :index do
     @product = Product.new
