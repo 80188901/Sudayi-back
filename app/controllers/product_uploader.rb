24d1915f59ccda0bc5fa993fdbf2727930f6cadd
simon <xsqfeather@163.com>
2014-12-09 11:54:46 +0800

    123

--- app/controllers/product_uploader.rb
+++ app/controllers/product_uploader.rb
@@ -24,7 +24,7 @@ end
    @image_item.product_id = params[:product_id]
    @image_item.url = params[:cover_url]
    @image_item.name = '封面'
-   @image_item.is_cover = 1
+   @image_item.iscover = 1
    @image_item.save
    @image_item.to_json
   end
