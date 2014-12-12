9d25c735a91d45fb50333bfea16cff7cf141507d
simon <xsqfeather@gmail.com>
2014-12-12 11:18:58 +0800

    store manager

---
+++ mobile_admin/helpers/store_manager_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Fancyshpv2
+  class MobileAdmin
+    module StoreManagerHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers StoreManagerHelper
+  end
+end
