dbf778d9c99d2e383fa452a883f03292a7de066e
simon <xsqfeather@gmail.com>
2014-12-12 14:48:36 +0800

    123

--- mobile_admin/controllers/store_manager.rb
+++ mobile_admin/controllers/store_manager.rb
@@ -13,10 +13,18 @@ Fancyshpv2::MobileAdmin.controllers :store_manager do
   end
 
   post 'new_store',:csrf_protection => false do
-    params[:area]
-    params[:url]
-    params[:name]
-    
+    @store = Store.new
+    @store.credit_url = params[:url]
+    @store.name = params[:warehouse_name]
+    @store.open_time_begin_day = params[:first_time]
+    @store.open_time_end_day = params[:last_time]
+    @store.open_time_in_one_week = params[:day]
+    @store.account_id = params[:uid]
+    @store.save
+    @store_address = StoreAddress.new
+    @store_address.store_id = @store._id
+    @store_address.area_id = params[:area_id]
+    @store_address.details = params[:warehouse_address]
   end
   
 end
