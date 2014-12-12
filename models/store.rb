2e16ba80ea1be554ecd7c29d5ca5a4cdade53f23
simon <xsqfeather@gmail.com>
2014-12-12 14:07:02 +0800

    123

--- models/store.rb
+++ models/store.rb
@@ -9,6 +9,10 @@ class Store
   # field <name>, :type => <type>, :default => <value>
   field :name, :type => String
   field :in_charge, :type => String
+  field :open_time, :type => String
+  field :open_time_in_one_week, :type=> String
+  mount_uploader :credit_url, StoreUploader
+  field :credit_url, :type => String
 
   # You can define indexes on documents using the index macro:
   # index :field <, :unique => true>
