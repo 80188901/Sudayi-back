601cae37728a3215a8722734ccc63bedf23f605a
simon <xsqfeather@126.com>
2014-12-23 23:10:30 +0800

    deleted:    metronic_admin/views/layout_horizontal_sidebar_menu.html

---
+++ metronic_admin/controllers/view_layout.rb
@@ -0,0 +1,27 @@
+Fancyshpv2::MetronicAdmin.controllers :view_layout do
+  
+
+  get :layout_horizontal_sidebar_menu do 
+    render 'layout_horizontal_sidebar_menu.erb'
+  end
+  # get :index, :map => '/foo/bar' do
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+
+end
