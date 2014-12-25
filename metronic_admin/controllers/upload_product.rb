93276cfce8b921ba27c2b4c871dcd672d2d7e20f
simon <xsqfeather@126.com>
2014-12-25 18:09:14 +0800

    132

---
+++ metronic_admin/controllers/upload_product.rb
@@ -0,0 +1,25 @@
+Fancyshpv2::MetronicAdmin.controllers :upload_product do
+  
+    get :index do
+      render('upload_product/index.erb', :layout => :normal)
+    end
+  #   session[:foo] = 'bar'
+  #   render 'index'
+  # end
+
+  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
+  #   case content_type
+  #     when :js then ...
+  #     else ...
+  # end
+
+  # get :foo, :with => :id do
+  #   'Maps to url '/foo/#{params[:id]}''
+  # end
+
+  # get '/example' do
+  #   'Hello world!'
+  # end
+  
+
+end
---
+++ metronic_admin/helpers/upload_product_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module Fancyshpv2
+  class MetronicAdmin
+    module UploadProductHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers UploadProductHelper
+  end
+end
